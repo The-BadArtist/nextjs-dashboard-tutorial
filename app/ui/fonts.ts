61ab4180a58c7686e8a075709e531e05d4d30854
The-BadArtist <javianhuell@gmail.com>
2025-05-25 21:52:13 -0400

    [UPDATE] Completed Tutorial

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,7 @@
-import { Inter } from 'next/font/google';
+// import { Inter } from 'next/font/google';
 import { Lusitana } from 'next/font/google';
 
-export const inter = Inter({subsets: ['latin']});
+// export const inter =  Inter({subsets: ['latin']}); //! Font cannot be resolved from server
 
 export const lusitana = Lusitana({
     weight: ["400", "700"],
